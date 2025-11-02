6f3936ecb68218c3a62633b462d6a6e15e7fe34d
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2025-11-02 22:44:39 +0500

    Error Fixing

--- EFRelations/Controllers/OneToOneController.cs
+++ EFRelations/Controllers/OneToOneController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EFRelations.Data;
+using EFRelations.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFRelations.Controllers
 {
@@ -7,7 +10,33 @@ namespace EFRelations.Controllers
     [ApiController]
 
     //we are doing the relations with navigation and conversion property
-    public class OneToOneController : ControllerBase
+    public class OneToOneController(AppDbContext context) : ControllerBase
     {
+        [HttpPost("add-user")]
+        public async Task<IActionResult> CreateUser(User user)
+        {
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet("get-user")]
+        public async Task<IActionResult> GetUser()
+        {
+            return Ok(await context.Users.Include(x => x.Profile).ToListAsync());
+        }
+        [HttpPost("add-profile")]
+        public async Task<IActionResult> CreateProfile(Profile profile)
+        {
+            context.Profiles.Add(profile);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpGet("get-profiles")]
+        public async Task<IActionResult> GetProfile()
+        {
+            return Ok(await context.Profiles.Include(x => x.User).ToListAsync());
+        }
+
     }
 }
--- EFRelations/Program.cs
+++ EFRelations/Program.cs
@@ -1,11 +1,12 @@
 using EFRelations.Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(s => s.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -14,6 +15,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapScalarApiReference();
     app.MapOpenApi();
 }
 
