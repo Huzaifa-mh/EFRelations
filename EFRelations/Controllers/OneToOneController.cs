73dd435d1c424593bcb71397912d5e8707531c02
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2025-11-04 12:09:37 +0500

    Adding User data transfer object
    i am using the data transfer object now because previosly i am getting the navigation loop error in the get request.

--- EFRelations/Controllers/OneToOneController.cs
+++ EFRelations/Controllers/OneToOneController.cs
@@ -1,4 +1,5 @@
 ﻿using EFRelations.Data;
+using EFRelations.DTOs;
 using EFRelations.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,11 +21,76 @@ namespace EFRelations.Controllers
             return Ok();
         }
 
+
         [HttpGet("get-user")]
         public async Task<IActionResult> GetUser()
         {
-            return Ok(await context.Users.Include(x => x.Profile).ToListAsync());
+            //return Ok(await context.Users.Include(x => x.Profile).ToListAsync()); //fixing this line with DTO
+
+            //return Ok(await context.Users.Include(x => x.Profile).Select(x =>  new UserDto
+            //{
+            //    Id = x.Id,
+            //    Username = x.Username,
+            //    Profile = x.Profile == null ? null : new ProfileDto
+            //    {
+            //        Id = x.Profile.Id,
+            //        Bio = x.Profile.Bio,
+            //        Username = x.Username
+            //    }
+            //})
+            //.ToListAsync());
+
+            var user = await context.Users.Include(x => x.Profile).Select(x => MapUser(x)).ToListAsync();
+
+            return Ok(user);
         }
+
+        [HttpGet("get-user/{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            if(id == null)
+            {
+                return BadRequest("The id is not defined properly");
+            }
+            //var user = await context.Users.Include(x => x.Profile).Where(x => x.Id == id).Select(x => new UserDto
+            //{
+            //    Id = x.Id,
+            //    Username = x.Username,
+            //    Profile = x.Profile == null ? null : new ProfileDto
+            //    {
+            //        Id = x.Profile.Id,
+            //        Bio = x.Profile.Bio,
+            //        Username = x.Username
+            //    }
+
+            //}).FirstOrDefaultAsync();
+
+            //we can do another this and make this code much better (b/c is been used by 2 request getuser and getuserbyid
+
+            var user = await context.Users.Include(x => x.Profile)
+                .Where(x => x.Id == id)
+                .Select(x => MapUser(x))
+                .FirstOrDefaultAsync();
+
+            return Ok(user);
+        }
+
+        private static UserDto MapUser(User x)
+        {
+            return new UserDto
+            {
+                Id = x.Id,
+                Username = x.Username,
+                Profile = x.Profile == null ? null : new ProfileDto
+                {
+                    Id = x.Profile.Id,
+                    Bio = x.Profile.Bio,
+                    Username = x.Username
+
+                }
+            };
+        }
+
         [HttpPost("add-profile")]
         public async Task<IActionResult> CreateProfile(Profile profile)
         {
---
+++ EFRelations/DTOs/OneToOneDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace EFRelations.DTOs
+{
+    public class OneToOneDTO
+    {
+    }
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+
+        //nested dto
+        public ProfileDto Profile { get; set; }
+    }
+    public class ProfileDto
+    {
+        public int Id { get; set; }
+        public string? Bio { get; set; }
+
+        public string? Username { get; set; }
+
+    }
+}
